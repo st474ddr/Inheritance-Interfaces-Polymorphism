568a4ad160928184bbb5a3d4d7902d00c31a8f00
st474ddr <l7365714@gmail.com>
2018-06-22 01:56:12 +0800

    Create Onetime.java

---
+++ Onetime.java
@@ -0,0 +1,12 @@
+
+public class Onetime extends Appointment{
+
+	public Onetime(int year, int month, int day, String description)
+	{
+	   super(year, month, day, description);
+	}
+	 public String toString() {
+	        return getYear() + "/" + getMonth() + "/" + getDay() + "  " + super.toString() + " is Onetime";
+	    }
+	
+}

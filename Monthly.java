3d480b7b7af11580120db4176ccb41ccbd668d69
st474ddr <l7365714@gmail.com>
2018-06-22 01:55:29 +0800

    Create Monthly.java

---
+++ Monthly.java
@@ -0,0 +1,26 @@
+
+public class Monthly extends Appointment{
+
+	public Monthly(int year, int month, int day, String description)
+	{
+	   super(year, month, day, description);
+	}
+	
+	//確保都是之後的預約
+	public boolean occursOn(int year, int month, int day)
+	{
+	   if (year < getYear())
+	   {
+	      return false;
+	   }
+	   if (month < getMonth() && year == getYear())
+	   {
+	      return false;
+	   }
+	   return day == getDay();
+	}
+
+	 public String toString() {
+	        return getYear() + "/" + getMonth() + "/" + getDay() + "  " + super.toString() + " is Monthly";
+	    }
+}
